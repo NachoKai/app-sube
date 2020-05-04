e5716930f20d56b7cd6202ac2c78992ee9dedbf7
NachoKai <nachokai@outlook.com>
2020-05-03 21:44:39 -0300

    Updates

---
+++ precache-manifest.4193395535cf89958c7b838a7daa71eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476eeb8a4b61bebb4237a71f86764f6f",
+    "url": "/app-sube/index.html"
+  },
+  {
+    "revision": "d34ecc3a530b24e15bd4",
+    "url": "/app-sube/static/css/main.6eda0126.chunk.css"
+  },
+  {
+    "revision": "f06edb32330ab3b7e9d9",
+    "url": "/app-sube/static/js/2.960bb3aa.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/app-sube/static/js/2.960bb3aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34ecc3a530b24e15bd4",
+    "url": "/app-sube/static/js/main.326a0c74.chunk.js"
+  },
+  {
+    "revision": "43faedd604d1fc5331c1",
+    "url": "/app-sube/static/js/runtime-main.cfde5dc0.js"
+  }
+]);
